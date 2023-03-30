7ac9a36d5482ac415ba489e08ed98e92df2f964c
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2023-03-29 20:09:15 -0600

    Update Ada1bot2.py

--- Ada1bot2.py
+++ Ada1bot2.py
@@ -1,6 +1,12 @@
 import discord
 import requests
 from bs4 import BeautifulSoup
+def codescrape():
+    page = requests.get("https://xurlocation.com/ada-1-mods-today-destiny-2/")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    weapons = soup.find_all('ul')[2]
+    weapons = weapons.text
+    return weapons
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 @client.event
@@ -12,9 +18,6 @@ async def on_message(message):
         return
  
     if message.content.startswith('!ada1'):
-        page = requests.get("https://xurlocation.com/ada-1-mods-today-destiny-2/")
-        soup = BeautifulSoup(page.content, 'html.parser')
-        weapons = soup.find_all('ul')[2]
-        weapons = weapons.text
+        weapons=codescrape()
         await message.channel.send(f'{weapons}')
 client.run('<YOUR BOT TOKEN HERE>')
